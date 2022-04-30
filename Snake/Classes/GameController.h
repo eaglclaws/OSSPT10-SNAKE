ed45e1c2dbf1fc2560b86d6269d8d324ad72ca53
alsgur7859 <alsgur7859@naver.com>
2022-04-30 14:35:39 +0900

    GameController.h modified(initialized) accurately

--- Snake/Classes/GameController.h
+++ Snake/Classes/GameController.h
@@ -1,28 +1,32 @@
 #pragma once
 #include <string>
+#include <vector>
+#include <utility>
 
 using namespace std;
 
 class GameController {
 
-	//score 기록
+private:
+	//가장 최근 score 저장
 	int score;
 
-	//랜덤위치에 food 배치
-	void food();
+	//랭킹 기록을 위한 username과 score 쌍들 (나중에 이걸 파일로 저장해야함.)
+	vector<pair<string, int>> ranking;
 
-	//food를 먹었는지 체크 (여기서 안 해도 됨.)
-	void checkFood();
+public:
+	//GameOverScene에서 GameController로부터 게임 종료 시점의 Score를 받아오기 위함.
+	void getScore();
 
-	//플레이어 이름
-	string playerName;
+	//Game에서 게임 종료 시점의 Score를 GameController로 받아오기 위함.
+	void setScore();
 
-	//랭킹 기록(int* 임시, 외부 파일로 해야 안날아감)
-	int* ranking;
+	//GameOverScene에서 입력받은 user를 score와 pair로 ranking에 저장하기 위함.
+	void adduser(string);
 
-	//게임 종료
-	void endGame();
+	//ranking에 있는 user와 score pair를 username을 인자로 받아 지우기 위함.
+	void deleteUser(string);
 
-	//종료 조건:자기 몸 건드렸는지, 벽에 닿았는지, 이동방향과 반대로 움직였는지, 
-
+	//종료 조건:자기 몸 건드렸는지, 이동방향과 반대로 움직였는지, 벽에 닿았는지 (추후 수정)
+	void endGameCondition();
 };
