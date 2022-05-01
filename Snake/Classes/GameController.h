0acbe2b8e63c0a05bbd4a2aa9ae57f0c41fd8e2c
alsgur7859 <alsgur7859@naver.com>
2022-05-01 21:04:41 +0900

    add recordRank()

--- Snake/Classes/GameController.h
+++ Snake/Classes/GameController.h
@@ -24,6 +24,9 @@ public:
 	//GameOverScene에서 입력받은 user를 score와 pair로 ranking에 저장하기 위함. (score 상위 10개만 남김.)
 	void addUser(string);
 
-	//종료 조건:자기 몸 건드렸는지, 이동방향과 반대로 움직였는지, 벽에 닿았는지 (추후 수정)
+	//랭킹이 프로그램을 종료해도 날아가지 않도록 텍스트 파일에 기록
+	void recordRank();
+
+	//종료 조건:자기 몸 건드렸는지, 벽에 닿았는지 (추후 수정) //종료조건은 이미 구현 됐고, 여기서는 application의 state변화를 이끌어 내는 것!
 	void endGameCondition();
 };
