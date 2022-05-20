f56a7ecca753eda419e3c3fe21020fe31f1f1aa1
alsgur7859 <alsgur7859@naver.com>
2022-05-20 13:51:35 +0900

    make AutoGame same with SoloGame

---
+++ Snake/Classes/AutoGame.cpp
@@ -0,0 +1,143 @@
+/*
+Copyright 2022 °í¼®Çö
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include <array>
+#include <vector>
+#include <utility>
+#include "Game.h"
+#include "AutoGame.h"
+#include "Board.h"
+
+AutoGame::AutoGame()
+{
+    state = AutoGame_STATE_INIT;
+    board = new Board;
+}
+
+AutoGame::~AutoGame()
+{
+    delete board;
+}
+
+void AutoGame:: init()
+{
+    state = AutoGame_STATE_INIT;
+    board->init();
+}
+
+void AutoGame::play()
+{
+    state = AutoGame_STATE_PLAY;
+}
+
+void AutoGame::pause()
+{
+    state = AutoGame_STATE_PAUSE;
+}
+
+void AutoGame::over()
+{
+    state = AutoGame_STATE_OVER;
+}
+
+enum game_state AutoGame::get_state()
+{
+    return state;
+}
+
+enum board_elements AutoGame::board_data(int x, int y)
+{
+    return board->data()->at(y).at(x);
+}
+
+enum board_dir AutoGame::get_direction()
+{
+    return board->get_direction();
+}
+
+enum AutoGame_state AutoGame::update()
+{
+    bool board_live = board->update();
+    if (!board_live) {
+        over();
+        return state;
+    }
+    else {
+        return state;
+    }
+}
+
+bool AutoGame::place_apple(int x, int y)
+{
+    if (board->data()->at(y).at(x) != EMPTY) {
+        return false;
+    }
+    else {
+        board->data()->at(y).at(x) = APPLE;
+        board->apple_placed();
+        return true;
+    }
+}
+
+bool AutoGame::is_apple_placed()
+{
+    return board->is_apple_placed();
+}
+
+void AutoGame::key_event(enum key_press ekey)
+{
+    switch (ekey) {
+    case KEY_UP:
+        board->set_direction(UP);
+        break;
+    case KEY_DOWN:
+        board->set_direction(DOWN);
+        break;
+    case KEY_LEFT:
+        board->set_direction(LEFT);
+        break;
+    case KEY_RIGHT:
+        board->set_direction(RIGHT);
+        break;
+    case KEY_ESC:
+        pause();
+        break;
+    }
+}
+
+int** AutoGame::export_board()
+{
+    return board->export_board();
+}
+
+std::vector<std::pair<int, int>>* AutoGame::export_snake()
+{
+    return board->export_snake();
+}
+
+int AutoGame::export_dir()
+{
+    return board->export_dir();
+}
+
+int AutoGame::player_score()
+{
+    return board->get_length();
+}
+
+void AutoGame::load(int** board_save, std::vector<std::pair<int, int>>* snake_save, int dir_save)
+{
+    board->load(board_save, snake_save, dir_save);
+}
---
+++ Snake/Classes/AutoGame.h
@@ -0,0 +1,67 @@
+/*
+Copyright 2022 °í¼®Çö
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef __AUTOGAME_H_
+#define __AUTOGAME_H_
+#include "Game.h"
+#include <vector>
+#include <utility>
+#include "Board.h"
+
+
+class AutoGame: public Game {
+private:
+    Board* board;
+    enum game_state state;
+public:
+    AutoGame();
+    ~AutoGame();
+    
+    enum game_state get_state() override;
+
+    void init() override;
+
+    void play() override;
+
+    void pause() override;
+
+    void over() override;
+
+    
+    enum game_state update() override;
+
+    enum board_elements board_data(int, int) override;
+
+    enum board_dir get_direction() override;
+
+    void key_event(enum key_press) override;
+    
+    bool place_apple(int, int) override;
+
+    int player_score() override;
+
+    int** export_board() override;
+
+    std::vector<std::pair<int, int>>* export_snake() override;
+
+    int export_dir() override;
+
+    void load(int**, std::vector<std::pair<int, int>>*, int) override;
+
+    bool is_apple_placed() override;
+};
+
+#endif
