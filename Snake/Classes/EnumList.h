4511db4ef891b09f26538e3baa1ef9c9e1a420de
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2022-05-20 18:34:41 +0900

    EnumList init

---
+++ Snake/Classes/EnumList.h
@@ -0,0 +1,21 @@
+#ifndef __ENUMLIST_H__
+#define __ENUMLIST_H__
+
+enum board_elements
+{
+    EMPTY,
+    WALL,
+    HEAD,
+    SNAKE,
+    TAIL,
+    APPLE
+};
+
+enum board_dir
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+#endif
