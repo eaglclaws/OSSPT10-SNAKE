e7504209ab0f1b2d8461db0bf2a7c8fae5d31270
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2022-05-25 19:53:56 +0900

    init EnumList.h

--- Snake/Classes/EnumList.h
+++ Snake/Classes/EnumList.h
@@ -16,6 +16,40 @@ enum board_dir
     UP,
     DOWN,
     LEFT,
-    RIGHT
+    RIGHT,
+    NONE_DIR
 };
+
+enum PlayerSelect {
+    PLAYER1,
+    PLAYER2,
+    NONE
+};
+
+enum game_state {
+    GAME_STATE_INIT,
+    GAME_STATE_PLAY,
+    GAME_STATE_PAUSE,
+    GAME_STATE_OVER
+};
+
+enum key_press {
+    KEY_UP,
+    KEY_DOWN,
+    KEY_LEFT,
+    KEY_RIGHT,
+    KEY_W,
+    KEY_A,
+    KEY_S,
+    KEY_D,
+    KEY_ESC
+};
+
+struct snake_node
+{
+    struct snake_node* next;
+    int x;
+    int y;
+};
+
 #endif
